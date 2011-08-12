1dc42d5daf650374595d6dcd1d7dbc9a23b65ab1
Renato Rodrigues <simpsonpt@gmail.com>
2011-08-12 17:59:38 +0200

    Sender (Mesh Broadcaster)

---
+++ sender.c
@@ -0,0 +1,44 @@
+/* Sender 0.2 @ CCC 2011 Germany
+ * Develop by 0xCC
+ * */
+#include "funk/nrf24l01p.h"
+#include "funk/mesh.h"
+#include "basic/basic.h"
+#include "basic/config.h"
+#include "lcd/lcd.h"
+#include "lcd/print.h"
+#include "usetable.h"
+
+void _mesh_sendloop(void);
+void _init(void);
+
+/* tx_msg MAX <= 20 */
+char tx_msg[20];
+
+/*Main Loop*/
+void ram(void)
+{
+	char inputed[20];
+    for(int i=0; i<20; i++) 
+		inputed[i] = 0;
+    input("Write msg:", inputed, ' ', '}', 20);
+    strcpy(tx_msg, inputed);
+    while(getInputRaw() != BTN_UP)
+    {
+        lcdClear();
+        lcdPrintln("Sending...");
+        lcdPrintln(tx_msg);
+        lcdPrintln("UP to Exit!");
+        lcdRefresh();
+        _mesh_sendloop();
+    }
+}
+
+/*Mesh Send*/
+void _mesh_sendloop(void)
+{
+    MPKT * mpkt= meshGetMessage('A');
+    MO_TIME_set(mpkt->pkt,getSeconds()+3600);
+    strcpy((char*)MO_BODY(mpkt->pkt), &tx_msg[0]);
+    getInputWaitTimeout(2000);
+}
